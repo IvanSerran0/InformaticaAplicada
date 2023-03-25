3fc98508dbb6702df8aabb96685c200092431db0
Ivan Serrano <36173319@uaz.edu.mx>
2023-03-25 16:10:41 -0600

    Update example

--- 00_Introduccion/ejercicio_1.py
+++ 00_Introduccion/ejercicio_1.py
@@ -1,31 +1,31 @@
 #Ejercicios 1
-#ESTO ES UNA PRUEBAAAAAA222!!!3333
+# Ivan S 
 # Introducción
 # Una expresión puede ser una operación aritmetica
 
-print(6 + 9)                       # 15
-print(3 - 1)                       # 
-print(1 * 1)                       # 
-print(8 / 5)                       # 
-print(2 ** 4)                      # 
-print(8 % 3)                       # 
-print(9 // 4)                      # 
-print(15<6)                        # 
-print(2.5 * 1.5 * 3)               # 
-print(2j + 2 + 3j + 5)             # 
-print('Hola mundo')                # 
-print(['Mar', 2, 5])               # 
-print({'Nombre':'Tu nombre'})      #
-print({'Algo', 2.67, 9.5})         # 
-print(('Lag', 6.8, 7.5, 9))        # 
+print(20 + 5)                       # 25
+print(500 - 350)                       #150 
+print(5 * 6)                       # 30
+print(20 / 2)                       # 10.0
+print(3 ** 4)                      # 81
+print(100 % 22)                       #12 
+print(5 // 12)                      # 0
+print(7<5)                        # False
+print(35.2 * 2 * 9.5)               # 668.8000000000001
+print(10j + 5 + 6j + 4)             # 9+16j
+print('Hola mundo')                # Hola mundo
+print(['Sol', 2, 5])               # ['Sol', 2, 5]
+print({'Nombre':'Ivan'})      # {'Nombre': 'Ivan'}
+print({'Holaa', 85.7, 91.2})         # {'Holaa', 91.2, 85.7}
+print(('FPS', 8.6, 6.4, 10))        # ('FPS', 8.6, 6.4, 10)
 
 # Tipos de datos
 
 print(type(15<6))                       # <class 'bool'>
-print(type(2.5 * 1.5 * 3))              #
-print(type(2j + 2 + 3j + 5))            #
-print(type('Hola mundo'))               # 
-print(type(['Mar', 2, 5]))            # 
-print(type({'Nombre':'Tu nombre'}))     # 
-print(type({'Algo', 2.67, 9.5}))        # 
-print(type(('Lag', 6.8, 7.5, 9)))       # 
+print(type(2.5 * 1.5 * 3))              # <class 'float'>
+print(type(2j + 2 + 3j + 5))            # <class 'complex'>
+print(type('Hola mundo'))               # <class 'str'>
+print(type(['Sol', 2, 5]))            # <class 'list'>
+print(type({'Nombre':'Ivan'}))     #  <class 'dict'>
+print(type({'Holaa', 85.7, 91.2}))        # <class 'set'>
+print(type(('FPS', 8.6, 6.4, 10)))       # <class 'tuple'>
