d7a9d9824a347a4d84f1d63972daa515cb52704b
Ivan Serrano <36173319@uaz.edu.mx>
2023-04-28 19:50:38 -0600

    Update Homework

--- Codigo_Tarea/Señales_analogicas_digitales.py
+++ Codigo_Tarea/Señales_analogicas_digitales.py
@@ -1,44 +1,43 @@
-# Señales analógicas
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy.io.wavfile as waves
+# Señales analógicas vamos a trabajar señales analogicas en esta primera parte
+import numpy as np #cargamos la libreria numpy para procesamiento numerico y la cargamos como np
+import matplotlib.pyplot as plt #cargamos la libreria para poder graficar en python y la cargamos como plt
+import scipy.io.wavfile as waves #libreria para lectura de archivos de audio tipo wav y la importamos como waves
 
 # INGRESO
-# archivo = input('archivo de audio: ')
-archivo = 'Alarm01.wav'
-muestreo, sonido = waves.read(archivo)
+archivo = 'Alarm01.wav' #Se procesa un archivo de audio (cabe recalcar que hay que poner el nombre del archivo y extension)
+muestreo, sonido = waves.read(archivo) #hay que obtener la veloccidad de muestreo y el sonido que obtenemos mediante la funcion waves del archivo
 
 # SALIDA - Observacion intermedia
-print('frecuencia de muestreo: ', muestreo)
-print('dimensiones de matriz: ', np.shape(sonido))
+print('frecuencia de muestreo: ', muestreo) #Nos mostrará la frecuencia del sonido guardado en la variable muestreo 22050
+print('dimensiones de matriz: ', np.shape(sonido)) #Nos mostrará cuantos canales de audio existen (122868,2)
 print('datos de sonido: ')
-print(sonido)
+print(sonido) #[[0 0]]
 
-# segmento de tiempo
-canal  = 0
-inicia = 2600
-termina = 2720
+# segmento de tiempo para visualizar en una gráfica
+canal  = 0 #canal que observaremos
+inicia = 2600 #desde que punto inicia nuestra observacion
+termina = 2720 #y donde termina la observacion
 
-# Extrae el segmento desde sonido
-dt = 1/muestreo
-ti  = np.arange(inicia*dt,termina*dt,dt)
-muestras = len(ti)
-segmento = sonido[inicia:inicia+muestras, canal]
+# Extrae el segmento desde sonido para visualizar en unidades de tiempo ya que lña señal es analogica
+dt = 1/muestreo #1/22050=4.5351
+ti  = np.arange(inicia*dt,termina*dt,dt) #Para interpretar los elementos de tiempo utilizamos este arreglo 
+muestras = len(ti) #La cantidad de muestras que vamos a observar corresponde al tamaño del vector ti
+segmento = sonido[inicia:inicia+muestras, canal] #El segmento del sonido que se va a observar corresponde al numero de muestras desde donde inicia hasta inicia+muestras, utilizando el canal seleccionado
 
 #SALIDA
-plt.plot(ti,segmento)
-plt.xlabel('t segundos')
-plt.ylabel('sonido(t)')
-plt.show()
+plt.plot(ti,segmento) #La grafica se obtiene con esta instruccion utilizando los valores de ti y el degmento seleccionado
+plt.xlabel('t segundos') #Etiqueta para el eje de las x
+plt.ylabel('sonido(t)') #Etiqueta para el eje y
+plt.show() #Es para observar en pantalla la gráfica y se observa una gráfica una señal que varia de forma oscilatoria del sonido en el segmento seleccionado
+
+# SALIDA - datos del segmento de audio
+print(segmento) #Se observan los valores de la gráfica del segmento seleccionado
 
 ############Señales Digitales
 
-# SALIDA - datos del segmento de audio
-print(segmento)
-
 # grafica segmento de sonido en forma discreta
 ni = np.arange(inicia,inicia+muestras,1)
-plt.stem(ni,segmento)
+plt.stem(ni,segmento) #Nos muestra la gráfica sin unir los puntos por una linea
 plt.xlabel('n')
 plt.ylabel('segmento sonido[n]')
 plt.show()
